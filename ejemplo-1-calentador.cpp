2a7c3954537a9be4126988fa7d084d818331089c
StephaneLvtc <jshc1xv97@gmail.com>
2018-04-26 15:26:46 +0000

    Farenheit

--- ejemplo-1-calentador.cpp
+++ ejemplo-1-calentador.cpp
@@ -8,8 +8,9 @@ public:
     Calentador(); //constructor
     void calentar();
     void enfriar();
-    void imprimeTemperatura();
-    int accedeTemperatura() const; // a los metodos de acceso se les agrega el const
+    void imprimeCentigrados();
+    void imprimeFarenheit() const;
+    int accedeTemperatura() const; // a los metodos de acceso se les agrega el const (metodos de solo lectura)
 };// es obligatorio al final de la clase
 
 //metodo constructor
@@ -20,13 +21,21 @@ Calentador::Calentador()
 }
 
 void Calentador::calentar(){ // los dos puntos indican que el metodo es de la clase calentador
-    temperatura += 5;
+    if(temperatura + incremento <= 30){
+        temperatura += incremento;
+    }
 }
 void Calentador::enfriar(){ // los dos puntos indican que el metodo es de la clase calentador
-    temperatura -= 5;
+    if(temperatura - incremento >= -10){
+        temperatura -= incremento;
+    }
 }
-void Calentador::imprimeTemperatura(){
+void Calentador::imprimeCentigrados(){
     std::cout<<"La temperatura es: "<< temperatura << std::endl; //std::endl se puede usar en lugar de \n
+}
+void Calentador::imprimeFarenheit() const { // no puedo modificar variables de instancia en los metodos que contienen const
+    std::cout << "La temperatura es " <<temperatura * 1.8 +32 
+                << " °F" <<  std::endl;
 }
 int Calentador::accedeTemperatura() const
 {
@@ -38,8 +47,11 @@ int main()
     Calentador c2;
     
     c1.calentar();
-    c1.imprimeTemperatura();
+    c1.imprimeCentigrados();
+    c1.imprimeFarenheit();
+    
     
     c2.enfriar();
-    c2.imprimeTemperatura();
+    c2.imprimeCentigrados();
+    c2.imprimeFarenheit();
 }
