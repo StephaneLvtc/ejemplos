cf23c99aa263aeec76486b0e90bf781fcd104745
StephaneLvtc <jshc1xv97@gmail.com>
2018-05-02 15:56:50 +0000

    Se agrego la sobrecarga de operadores

--- ejemplo-1-calentador.cpp
+++ ejemplo-1-calentador.cpp
@@ -9,11 +9,12 @@ private:
 public:
     Calentador(int min, int max, int temperatura = 0); //constructor 
                                                         // si en la temperatura no me dan un valor toma el valor de cero
-    void calentar();
-    void enfriar();
-    void imprimeCentigrados();
+    void operator++();
+    void operator--();
+    void imprimeCentigrados() const;
     void imprimeFarenheit() const;
     int accedeTemperatura() const; // a los metodos de acceso se les agrega el const (metodos de solo lectura)
+    bool operator==(Calentador otro); // Sobrecarga del metodo miembro
 };// es obligatorio al final de la clase
 
 //metodo constructor
@@ -33,17 +34,17 @@ Calentador::Calentador( int min, int max, int temperatura)
     this->min = min;
     this->max = max;
 }
-void Calentador::calentar(){ // los dos puntos indican que el metodo es de la clase calentador
+void Calentador::operator++(){ // los dos puntos indican que el metodo es de la clase calentador
     if(temperatura + incremento <= this->max){
         temperatura += incremento;
     }
 }
-void Calentador::enfriar(){ // los dos puntos indican que el metodo es de la clase calentador
+void Calentador::operator--(){ // los dos puntos indican que el metodo es de la clase calentador
     if(temperatura - incremento >= this->min){
         temperatura -= incremento;
     }
 }
-void Calentador::imprimeCentigrados(){
+void Calentador::imprimeCentigrados() const{
     std::cout<<"La temperatura es: "<< temperatura << std::endl; //std::endl se puede usar en lugar de \n
 }
 void Calentador::imprimeFarenheit() const { // no puedo modificar variables de instancia en los metodos que contienen const
@@ -54,17 +55,34 @@ int Calentador::accedeTemperatura() const
 {
     return temperatura;
 }
+bool Calentador::operator==(Calentador otro)
+{
+     if(this->temperatura==otro.temperatura){
+         return true;
+     }
+     else{
+         return false;
+     }
+}
 int main()
 {
    // try{
         Calentador c1{-10,10};//los parametros van entre llaves
-        Calentador c2{30, 0, -10};
-        
-        c1.calentar();
+        Calentador c2{0, 30, 10};
+        //c1 == c2 es equivalente a c1.operator==(c2)
+        if(c1==c2){
+            std::cout <<"Iguales\n" ;
+        }
+        else {
+            std::cout <<"No son Iguales\n";
+        }
+        //++c1 es equivalente a c1.operator++();
+        ++c1;
         c1.imprimeCentigrados();
         c1.imprimeFarenheit();
         
-        c2.enfriar();
+        //--c2 es equivalente a c2.operator--();
+        --c2;
         c2.imprimeCentigrados();
         c2.imprimeFarenheit();
   //  }
